8b33bfe7118fed9610007181fb1676b4963b29bd
ghazalehsamoudi <50411257+ghazalehsamoudi@users.noreply.github.com>
2019-05-25 14:01:20 +0430

    Add files via upload

---
+++ 05.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+	int age;
+	cin>>age;
+	if(age>18)
+	    cout<<"age is greater than 18";
+	else
+	    cout<<"age is less than 18";
+}
